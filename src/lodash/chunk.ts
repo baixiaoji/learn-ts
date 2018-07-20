d1e1dda7e6b96d0745f828c2329b5c00357a28a1
baixiaoji <1041874421@qq.com>
2018-07-20 14:08:15 +0800

    feat: 完成了lodash chunk 函数的功能

--- src/lodash/chunk.ts
+++ src/lodash/chunk.ts
@@ -3,11 +3,8 @@
  * array
  * size = 1
  */
-function chunk<T>(a: T[], size: number = 1): T[] | T[][]{
+function chunk<T>(a: T[], size: number = 1): T[][]{
     const time = Math.ceil(a.length / size);
-    if (time === 1) {
-        return a;
-    }
     const array = [];
     for(let i = 0; i < time; i++){
         array.push(a.splice(0, size))
@@ -15,7 +12,9 @@ function chunk<T>(a: T[], size: number = 1): T[] | T[][]{
     return array;
 }
 
-console.log(chunk([1,2,3], 3))
-console.log(chunk([1,2,3,4], 2))
-console.log(chunk([1,2,3,4], 3))
-console.log(chunk([1,2,3,4], 1))
+// console.log(chunk<number>([1,2,3], 3))
+// console.log(chunk([1,2,3,4], 2))
+console.log(chunk<number | string>([1,2,'3',4], 3))
+// console.log(chunk([1,2,3,4], 1))
+
+const a: string[] = ['1', '2', '3']
